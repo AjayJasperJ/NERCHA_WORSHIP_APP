abe40478314e624ff5a7b721d21e44c3d765baf2
AjayJasperJ <ajayjasperjames@gmail.com>
2025-05-14 09:08:13 +0530

    directory added

--- lib/core/constants/images.dart
+++ lib/core/constants/images.dart
@@ -1,3 +1,10 @@
-class Constantimages {}
+class Constantimages {
+  final app_logo = 'assets/images/app_logo.png';
+  final onboarding_1 = 'assets/images/onboarding_1.png';
+  final onboarding_2 = 'assets/images/onboarding_2.png';
+  final onboarding_3 = 'assets/images/onboarding_3.png';
+  final google_logo = 'assets/images/google_logo.png';
+  final facebook_logo = 'assets/images/facebook_logo.png';
+}
 
 class Constanticons {}
