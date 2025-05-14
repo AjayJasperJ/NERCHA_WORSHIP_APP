0c21ae4407146c809919241ea5b7df3747716f01
AjayJasperJ <ajayjasperjames@gmail.com>
2025-05-14 08:56:46 +0530

    directory added

--- lib/core/constants/colors.dart
+++ lib/core/constants/colors.dart
@@ -1,1 +1,18 @@
-class Constantcolors {}
+import 'package:flutter/material.dart';
+
+class Constantcolors {
+  /*  Orange  */
+  Color nercha_orange = Color.fromRGBO(255, 250, 246, 1);
+  Color nercha_orange_1 = Color.fromRGBO(250, 132, 101, 1);
+  Color nercha_orange_2 = Color.fromRGBO(247, 79, 34, 1);
+
+  /*  Blue  */
+  Color nercha_blue = Colors.lightBlue;
+  Color nercha_darkblue = Color.fromRGBO(30, 52, 90, 1);
+
+  /*  Grey  */
+  Color nercha_grey = Color.fromRGBO(204, 204, 204, 1);
+
+  /*  White  */
+  Color nercha_white = Colors.white;
+}
