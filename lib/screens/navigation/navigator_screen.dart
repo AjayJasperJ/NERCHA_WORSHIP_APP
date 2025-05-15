c7293d3ef4aeb93050f2bebfbb6eb2e4352c1819
AjayJasperJ <ajayjasperjames@gmail.com>
2025-05-15 12:20:55 +0530

    navigation bar updated

--- lib/screens/navigation/navigator_screen.dart
+++ lib/screens/navigation/navigator_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:nercha_worship_app/app.dart';
 import 'package:nercha_worship_app/core/constants/colors.dart';
 import 'package:nercha_worship_app/core/constants/images.dart';
 import 'package:nercha_worship_app/screens/navigation/account/account_screen.dart';
@@ -34,12 +35,16 @@ class _NavigatorScreenState extends State<NavigatorScreen> {
   Widget build(BuildContext context) {
     List<Map<String, dynamic>> navigatorfield_data = [
       {'icon': icons.home, 'selectedicon': icons.home_fill, 'title': 'Home'},
-      {'icon': icons.search, 'selectedicon': icons.search, 'title': 'Home'},
-      {'icon': icons.event, 'selectedicon': icons.event_fill, 'title': 'Home'},
+      {'icon': icons.search, 'selectedicon': icons.search, 'title': 'Search'},
+      {
+        'icon': icons.event,
+        'selectedicon': icons.event_fill,
+        'title': 'Bookings',
+      },
       {
         'icon': icons.setting,
         'selectedicon': icons.setting_fill,
-        'title': 'Home',
+        'title': 'Account',
       },
     ];
     return LayoutBuilder(
@@ -78,7 +83,12 @@ class _NavigatorScreenState extends State<NavigatorScreen> {
                         : 0,
                     (index) {
                       final value = navigatorfield_data[index];
-                      return Image.asset(value['selectedicon']);
+                      return GestureDetector(
+                        onTap: () {
+                          onTabTapped(index);
+                        },
+                        child: _navigationIcon(value, index),
+                      );
                     },
                   ),
                 ),
@@ -87,6 +97,47 @@ class _NavigatorScreenState extends State<NavigatorScreen> {
           ],
         );
       },
+    );
+  }
+
+  Widget _navigationIcon(Map<String, dynamic> icondata, int index) {
+    final bool isSelected = currentIndex == index;
+    return GestureDetector(
+      onTap: () {
+        onTabTapped(index);
+      },
+      child: Container(
+        color: colors.nercha_white.withValues(alpha: 0),
+        width: displaysize.width * .18,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            isSelected
+                ? Image.asset(
+                  icondata['selectedicon'],
+                  color: colors.nercha_orange_2,
+                  height: displaysize.height * .028,
+                )
+                : Image.asset(
+                  icondata['icon'],
+                  color: colors.nercha_darkblue,
+                  height: displaysize.height * .028,
+                ),
+            SizedBox(height: displaysize.height * .003),
+            isSelected
+                ? Text(
+                  icondata['title'],
+                  style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                    fontFamily: 'general_sans',
+                    fontWeight: FontWeight.w500,
+                    fontSize: displaysize.height * .013,
+                    color: colors.nercha_orange_2,
+                  ),
+                )
+                : SizedBox.shrink(),
+          ],
+        ),
+      ),
     );
   }
 }
